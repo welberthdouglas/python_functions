2d8dc5e77cd09063210e67b45c2a88551c5eb145
Welberth Douglas <42102076+welberthdouglas@users.noreply.github.com>
2019-05-05 13:53:40 -0300

    Update decile.py

--- decile.py
+++ decile.py
@@ -13,22 +13,22 @@ def decile(df0):
     df[2] = 1-df.iloc[:,0] # creating the negative response column
     
     df.columns = ['resp','score','neg_resp']
-    df['decile'] = pd.qcut(df.iloc[:,1],10) #defining deciles
-    df = df.groupby('decile', as_index = False)
+    df['decile'] = pd.qcut(df.iloc[:,1],10) # defining deciles
+    df = df.groupby('decile', as_index = False) # aggregating data
 
     agg = pd.DataFrame()
-    agg['min_' + str(df0.columns[1])] = df.min().score
-    agg['max_' + str(df0.columns[1])] = df.max().score
+    agg['min_' + str(df0.columns[1])] = df.min().score.apply('{0:.3f}'.format)
+    agg['max_' + str(df0.columns[1])] = df.max().score.apply('{0:.3f}'.format)
     agg[df0.columns[0]] = df.sum().resp
     agg['n_'+ str(df0.columns[0])] = df.sum().neg_resp
-    agg['total'] = agg[df0.columns[0]] + agg['n_'+ str(df0.columns[0])]
-    agg[str(df0.columns[0]) + ' ratio'] = (agg[df0.columns[0]] / agg.total).apply('{0:.2%}'.format)
+    agg['total'] = (agg[df0.columns[0]] + agg['n_'+ str(df0.columns[0])])
+    agg[str(df0.columns[0]) + '_ratio'] = (agg[df0.columns[0]] / agg.total).apply('{0:.1%}'.format)
     
 
     agg = (agg.sort_values(by = 'max_' + str(df0.columns[1]), ascending = False)).reset_index(drop = True)
  
-    agg['gain'] = (agg[df0.columns[0]].cumsum()/agg[df0.columns[0]].sum())*100
-    agg['lift'] = agg.gain/((agg.total.cumsum()/agg.total.sum())*100)
+    agg['gain'] = (agg[df0.columns[0]].cumsum()/agg[df0.columns[0]].sum()).apply('{0:.1%}'.format)
+    agg['lift'] = ((agg[df0.columns[0]].cumsum()/agg[df0.columns[0]].sum())/(agg.total.cumsum()/agg.total.sum())).apply('{0:.2f}'.format)
     agg['KS'] = np.round(((agg[df0.columns[0]] / agg[df0.columns[0]].sum()).cumsum() - (agg['n_'+ str(df0.columns[0])] / agg['n_'+ str(df0.columns[0])].sum()).cumsum()), 4) * 100
     
     mark = lambda x: '◄─    ' if x == agg.KS.max() else ''
