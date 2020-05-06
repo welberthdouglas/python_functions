19fa891eddeaccfce13e626e326d58778c5e94fa
Welberth Douglas <42102076+welberthdouglas@users.noreply.github.com>
2020-05-06 01:19:59 -0300

    Update one_hot_decoder.py

--- one_hot_decoder.py
+++ one_hot_decoder.py
@@ -1,7 +1,7 @@
 def onehot_declutter(df, col_in:str, sep:str =','):
     """ transforms a column with multiple string values into one hot encoding"""
     
-    string_instances = {item.strip() for i in data.manag_problems for item in i.split(sep)}
+    string_instances = {item.strip() for i in data[col_in] for item in i.split(sep)}
     
     for string in string_instances:
         df[string.replace(' ','_')] = [1 if string in i else 0 for i in df[col_in]]
