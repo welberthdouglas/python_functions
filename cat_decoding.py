794027d373fde104f42b0adb0ee4a3c0effb1101
welberthdouglas <welberthdouglas@gmail.com>
2020-06-08 00:32:29 -0300

    add cat_decoding

---
+++ cat_decoding.py
@@ -0,0 +1,19 @@
+def cat_encoding(df, cat_vars:str, encoding:str = 'label'):
+    """Encodes cathegorical variables of a dataframe
+    
+    inputs
+    df - dataframe that have variables to encode
+    cat_vars - list of variables to encode
+    encoding - encoding method (possible values are 'label' or 'one_hot')
+    
+    output
+    df_out - outputs the modified dataframe (with the original columns replaced by the respective
+            encoded ones)"""
+    
+    if encoding == 'label':
+        df_cat = pd.DataFrame({col: df[col].astype('category').cat.codes for col in df[cat_vars]}, index=df.index)
+        df_out = df.drop(cat_vars, axis=1).merge(df_cat,left_index = True,right_index=True)
+    if encoding == 'one_hot':
+        df_out = pd.get_dummies(df,columns = cat_vars)
+        
+    return df_out
---
+++ corr_p_values.py
@@ -0,0 +1,12 @@
+def calculate_pvalues(df,method:str):
+    df = df.dropna()._get_numeric_data()
+    dfcols = pd.DataFrame(columns=df.columns)
+    pvalues = dfcols.transpose().join(dfcols, how='outer')
+    for r in df.columns:
+        for c in df.columns:
+            if method == 'pearson':
+                pvalues[r][c] = round(pearsonr(df[r], df[c])[1], 4)
+            if method == 'spearman':
+                pvalues[r][c] = round(spearmanr(df[r], df[c])[1], 4)
+            
+    return pvalues
