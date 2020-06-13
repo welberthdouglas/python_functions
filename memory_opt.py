d7ec00262ed71ac3ac5def3633063d79517e2bf9
welberthdouglas <welberthdouglas@gmail.com>
2020-06-13 12:39:08 -0300

    memory optimization pd.DataFrames added

---
+++ memory_opt.py
@@ -0,0 +1,32 @@
+def memory_opt(df:pd.DataFrame, datetime_features:list = [], threshold:float = 0.1):
+    """ reduces memory usage of a pandas dataframe"""
+    
+    m_b = df.memory_usage(deep = True).sum() / 1024**2
+    
+    # downcasting floats
+    for i in df.select_dtypes(include='float').columns:
+        df[i] = pd.to_numeric(df[i],downcast = 'float')
+        
+        if (df[i]%1 == 0).any():
+            df[i] = pd.to_numeric(df[i],downcast = 'integer')
+    
+    # downcasting ints
+    for i in df.select_dtypes(include=['int8','int64','int32']).columns:
+        
+        df[i] = pd.to_numeric(df[i],downcast = 'integer')
+        
+    # downcasting objects
+    for col in df.select_dtypes(include=['object']).columns:
+        if col not in datetime_features:
+            num_unique_values = len(df[col].unique())
+            num_total_values = len(df[col])
+            if float(num_unique_values) / num_total_values  < threshold:
+                df[col] = df[col].astype('category')
+        else:
+            df[col] = pd.to_datetime(df[col])
+        
+    m_a = df.memory_usage(deep = True).sum() / 1024**2
+    
+    
+    print(f'dataframe memory usage before optimization : {m_b} Mb')
+    print(f'dataframe memory usage : {m_a} Mb')
