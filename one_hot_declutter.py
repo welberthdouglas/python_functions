0eb3930cbaa95d175386bcb57a0bc2a80778b596
welberthdouglas <welberthdouglas@gmail.com>
2020-07-14 23:11:26 -0300

    refactoring onehotdeclutter

--- one_hot_declutter.py
+++ one_hot_declutter.py
@@ -1,7 +1,16 @@
 import pandas as pd
 
-def onehot_declutter(df:pd.DataFrame, col_in:str, sep:str =',', replace = True) -> pd.DataFrame:
-    """ transforms a column with multiple string values into one hot encoding"""
+def oneHotDeclutter(df:pd.DataFrame, col_in:str, sep:str =',', replace = True) -> pd.DataFrame:
+    """ transforms a column with multiple string values into one hot encoding
+    
+    Args
+    df : dataframe to execute the operation
+    col_in : column to declutter
+    replace : option to replace the original column by the decluttered one (default = True)      
+      
+    Return
+    df : Dataframe with decluttered column     
+    """
     
     string_instances = {item.strip() for i in data[col_in] for item in i.split(sep)}
     
@@ -9,4 +18,5 @@ def onehot_declutter(df:pd.DataFrame, col_in:str, sep:str =',', replace = True) 
         df[string.replace(' ','_')] = [1 if string in i else 0 for i in df[col_in]]
     if replace:
         df.drop(columns = col_in, inplace = True)
+
     return df
