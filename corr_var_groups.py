e3566c7dc2c9b9d01423532383b87e3474bc02f5
welberthdouglas <welberthdouglas@gmail.com>
2020-07-16 23:31:37 -0300

    fix corrVars

--- corr_var_groups.py
+++ corr_var_groups.py
@@ -6,25 +6,19 @@ def corrGroups(df:pd.DataFrame,corr_tresh:float=0.9) -> list:
     
     corrMatrix = df.corr().abs()
     corrMatrix.loc[:,:] =  np.tril(corrMatrix, k=-1)
-
-    already_in = set()
-    result = []
-    for col in corrMatrix:
-        tresh_corr = corrMatrix[col][corrMatrix[col] >= corr_tresh].index.tolist()
-        if tresh_corr and col not in already_in:
-            already_in.update(set(tresh_corr))
-            tresh_corr.append(col)
-            result.append(tresh_corr)
-    return result
-
-
-
-WIP:
-
-corr_tresh=0.7
+    corrMatrix = corrMatrix[corrMatrix > corr_tresh].dropna(how='all').dropna(axis=1,how='all')
+    corrMatrix['corr_groups'] = corrMatrix.apply(lambda x:sum([[x.name],x.index[x.notna()].tolist()],[]), axis=1)
+    corrMatrix['max'] = corrMatrix.max(axis=1)
     
-
-corrMatrix = df.corr().abs()
-corrMatrix.loc[:,:] =  np.tril(corrMatrix, k=-1)
-corrMatrix = corrMatrix[corrMatrix > corr_tresh].dropna(how='all').dropna(axis=1,how='all')
-corrMatrix['corr_groups'] = corrMatrix.apply(lambda x:sum([[x.name],x.index[x.notna()].tolist()],[]), axis=1)
+    corrVars = [i for i in corrMatrix.sort_values('max',ascending=False).corr_groups]
+    
+    remove=[]
+    for i in corrVars:
+        for j in range(0,len(corrVars)):
+            if set(i).issubset(corrVars[j]) and i!=corrVars[j] and i not in remove:
+                remove.append(i)
+                
+    for rm in remove:
+        corrVars.remove(rm)
+        
+    return corrVars
